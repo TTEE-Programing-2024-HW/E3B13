daf73a3f92a3df66745b1d21d095399d1eb0dd84
chensuuyuan <u11002213@o365.ttu.edu.tw>
2024-06-01 11:33:54 +0800

    檢測學生資料是否超出範圍

--- E3B13HW4.c
+++ E3B13HW4.c
@@ -89,3 +89,48 @@ printf("--------------------\n");
 			}
 			system("cls");
 			printf("輸入學生成績\n");
+			for (i = 0;i < n;i++)												//要求使用者輸入學生資料並檢測是否超出範圍 
+			{
+				printf("第%d位學生\n學生姓名：", i+1);							//姓名 
+				scanf("%s", &stu[i].name);
+				printf("學號6位數：");									//學號 
+				scanf("%d", &stu[i].id);
+				if (stu[i].id > 999999 || stu[i].id < 100000)
+					while (stu[i].id > 999999 || stu[i].id < 100000)
+					{
+						printf("\n輸入錯誤");
+						scanf("%6d", &stu[i].id);
+					}
+				printf("0<=成績<=100 \n");	
+				printf("\n數學成績");									//數學成績 
+				scanf("%d", &stu[i].math);
+				if (stu[i].math > 100 || stu[i].math < 0)
+					while (stu[i].math > 100 || stu[i].math < 0)
+					{
+						printf("\n輸入錯誤");
+						scanf("%d", &stu[i].math);
+					}
+				printf("物理成績");									//物理成績 
+				scanf("%d", &stu[i].physics);
+				if (stu[i].physics > 100 || stu[i].physics < 0)
+					while (stu[i].physics > 100 || stu[i].physics < 0)
+					{
+						printf("\n輸入錯誤");
+						scanf("%d", &stu[i].physics);
+					}
+				printf("英文成績");									//英文成績 
+				scanf("%d", &stu[i].english);
+				if (stu[i].english > 100 || stu[i].english < 0)
+					while (stu[i].english > 100 || stu[i].english < 0)
+					{
+						printf("\n輸入錯誤");
+						scanf("%d", &stu[i].english);
+					}
+				stu[i].avg = (float)(stu[i].math + stu[i].physics + stu[i].english) / 3;	//計算平均 
+				printf("\n");
+			}
+			
+			printf("回到主選單\n");
+			system("pause");
+			system("cls");
+			goto menu;
